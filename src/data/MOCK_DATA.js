f41164a4feb9ba313dfc70a1c76c80bf9bc5e8a9
okdol1 <okdol0611@gmail.com>
2022-12-05 02:28:47 +0900

    feat: 고유 id 추가

--- src/data/MOCK_DATA.js
+++ src/data/MOCK_DATA.js
@@ -1,48 +1,116 @@
+import { v4 as uuidv4 } from "uuid";
+
 const MOCK_DATA = [
   {
+    id: uuidv4(),
     name: "Category 1",
     folders: [
       {
+        id: uuidv4(),
         name: "Folder 1",
         files: null,
       },
       {
+        id: uuidv4(),
         name: "Folder 2",
         files: [
-          "File 1",
-          "File 2",
-          "File 3",
-          "File 4",
-          "File 5",
-          "File 6",
-          "File 7",
-          "File 8",
-          "File 9",
-          "File 10",
+          {
+            id: uuidv4(),
+            name: "File 1",
+          },
+          {
+            id: uuidv4(),
+            name: "File 2",
+          },
+          {
+            id: uuidv4(),
+            name: "File 3",
+          },
+          {
+            id: uuidv4(),
+            name: "File 4",
+          },
+          {
+            id: uuidv4(),
+            name: "File 5",
+          },
+          {
+            id: uuidv4(),
+            name: "File 6",
+          },
+          {
+            id: uuidv4(),
+            name: "File 7",
+          },
+          {
+            id: uuidv4(),
+            name: "File 8",
+          },
+          {
+            id: uuidv4(),
+            name: "File 9",
+          },
+          {
+            id: uuidv4(),
+            name: "File 10",
+          },
         ],
       },
     ],
   },
   {
+    id: uuidv4(),
     name: "Category 2",
     folders: [
       {
+        id: uuidv4(),
         name: "Folder 1",
         files: null,
       },
       {
+        id: uuidv4(),
         name: "Folder 2",
         files: [
-          "File 1",
-          "File 2",
-          "File 3",
-          "File 4",
-          "File 5",
-          "File 6",
-          "File 7",
-          "File 8",
-          "File 9",
-          "File 10",
+          {
+            id: uuidv4(),
+            name: "File 1",
+          },
+          {
+            id: uuidv4(),
+            name: "File 2",
+          },
+          {
+            id: uuidv4(),
+            name: "File 3",
+          },
+          {
+            id: uuidv4(),
+            name: "File 4",
+          },
+          {
+            id: uuidv4(),
+            name: "File 5",
+          },
+          {
+            id: uuidv4(),
+            name: "File 6",
+          },
+          {
+            id: uuidv4(),
+            name: "File 7",
+          },
+          {
+            id: uuidv4(),
+            name: "File 8",
+          },
+          {
+            id: uuidv4(),
+            name: "File 9",
+          },
+          {
+            id: uuidv4(),
+            name: "File 10",
+          },
         ],
       },
     ],
